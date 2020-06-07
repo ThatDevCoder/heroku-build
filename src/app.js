4bd49240d9640dd2879c15fe8479ba3ded2bbba2
Devyash <bordia999@gmail.com>
2020-06-07 12:10:45 +0530

    added dynamic port for heroku

--- src/app.js
+++ src/app.js
@@ -100,7 +100,7 @@ app.get('*',(req,res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is up and running on PORT 3000');
     
 })
